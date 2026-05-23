04d886e78254df6a9eb6be8166b4dfd7c0ab61e0
Jimmy <leitotapia07@gmail.com>
2026-05-23 17:17:27 -0500

    feat: Creacion de clase InventoryObserver

---
+++ src/observer/InventoryObserver.java
@@ -0,0 +1,11 @@
+package observer;
+
+public class InventoryObserver implements OrderObserver {
+
+    @Override
+    public void update(String message) {
+        System.out.println("Actualizando inventario...");
+    }
+
+
+}
