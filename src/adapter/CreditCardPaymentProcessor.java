d5ab6252a3b859be1f814a69903cdd33d5714bb1
Jimmy <leitotapia07@gmail.com>
2026-05-23 16:54:58 -0500

    feat: Creacion de clase CreditCardPaymentProcessor

---
+++ src/adapter/CreditCardPaymentProcessor.java
@@ -0,0 +1,19 @@
+package adapter;
+
+public class CreditCardPaymentProcessor implements PaymentProcessor {
+
+
+
+
+
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Pago realizado con Tarjeta de Crédito por: S/" + amount);
+    }
+
+
+
+
+
+}
